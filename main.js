c3eb008873969e7b2f9afdfdc0f6ce96c0c159c7
lennin_roman <lenninr96@gmail.com>
2024-06-28 23:19:42 -0400

    Funcion Leer finalizada JS

--- main.js
+++ main.js
@@ -34,3 +34,40 @@ function crear(e) {
     leer();
     console.log("Libro guardado correctamente");
 }
+
+document.getElementById("formulario").addEventListener("submit", crear);
+
+// Función Crear
+function crear(e) {
+    e.preventDefault();
+
+    let titulo = document.getElementById("titulo").value.trim();
+    let descripcion = document.getElementById("descripcion").value.trim();
+    let precio = document.getElementById("precio").value.trim();
+
+    // Verificar que los campos del formulario no esten vacios
+    if (titulo === '' || descripcion === '' || precio === '') {
+        alert('Todos los campos son obligatorios.');
+        return;
+    }
+
+    let libro = {
+        titulo,
+        descripcion,
+        precio
+    };
+
+
+    if (localStorage.getItem("Libros") === null) {
+        let libros = [];
+        libros.push(libro);
+        localStorage.setItem("Libros", JSON.stringify(libros));
+    } else {
+        let libros = JSON.parse(localStorage.getItem("Libros"));
+        libros.push(libro);
+        localStorage.setItem("Libros", JSON.stringify(libros));
+    }
+    document.getElementById("formulario").reset();
+    leer();
+    console.log("Libro guardado correctamente");
+}
