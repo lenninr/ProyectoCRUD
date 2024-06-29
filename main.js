f5b8102c7587c5e95f80e617c12a10b20c6223bb
lennin_roman <lenninr96@gmail.com>
2024-06-29 00:21:41 -0400

    cambios realizados a funciones CRUD JS

--- main.js
+++ main.js
@@ -35,42 +35,27 @@ function crear(e) {
     console.log("Libro guardado correctamente");
 }
 
-document.getElementById("formulario").addEventListener("submit", crear);
+//Funcion Leer
+function leer(){
+    let libros = JSON.parse(localStorage.getItem("Libros"));
+    document.getElementById("tbody").innerHTML = ""
+    for(let i=0; i < libros.length; i++){
+        let titulo = libros[i].titulo
+        let descripcion = libros[i].descripcion
+        let precio = libros[i].precio
 
-// Función Crear
-function crear(e) {
-    e.preventDefault();
+        document.getElementById("tbody").innerHTML +=
+        `<tr>
+            <td>${titulo}</td>
+            <td> ${descripcion} </td>
+            <td> ${precio} </td>
+            <td><button onclick="eliminar('${titulo}')" class="btn btn-danger">Eliminar</button></td>
+            <td><button onclick="editar('${titulo}')" class="btn btn-success">Editar</button></td>
 
-    let titulo = document.getElementById("titulo").value.trim();
-    let descripcion = document.getElementById("descripcion").value.trim();
-    let precio = document.getElementById("precio").value.trim();
-
-    // Verificar que los campos del formulario no esten vacios
-    if (titulo === '' || descripcion === '' || precio === '') {
-        alert('Todos los campos son obligatorios.');
-        return;
+        </tr>`
     }
-
-    let libro = {
-        titulo,
-        descripcion,
-        precio
-    };
-
-
-    if (localStorage.getItem("Libros") === null) {
-        let libros = [];
-        libros.push(libro);
-        localStorage.setItem("Libros", JSON.stringify(libros));
-    } else {
-        let libros = JSON.parse(localStorage.getItem("Libros"));
-        libros.push(libro);
-        localStorage.setItem("Libros", JSON.stringify(libros));
-    }
-    document.getElementById("formulario").reset();
-    leer();
-    console.log("Libro guardado correctamente");
 }
+
 
 //Funcion Editar
 function editar(titulo) {
@@ -79,11 +64,11 @@ function editar(titulo) {
         if (libros[i].titulo === titulo) {
             document.getElementById("body").innerHTML =
                 `
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-md-5">
                     <div class="card">
                         <div class="card-header">
-                            <h2>Editar libro</h2>
+                            <h2 class="text-center h5">Editar libro</h2>
                         </div>
                         <div class="card-body">
                             <form>
@@ -98,7 +83,7 @@ function editar(titulo) {
                                 </div>
                             </form>
                             <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
-                            <button class="btn btn-primary" onclick="VistaPrincipal()">Cancelar</button>
+                            <button class="btn btn-primary" onclick="vistaPrincipal()">Cancelar</button>
                         </div>
                     </div>
 
@@ -136,48 +121,48 @@ function eliminar(titulo){
 function vistaPrincipal(){
     document.getElementById("body").innerHTML =
     `
-    <div class="row">
-            <div class="col-md-5">
-                <div class="card">
-                    <div class="card-header">
-                        <h2>Agregar nuevo libro</h2>
-                    </div>
-                    <div class="card-body">
-                        <form id="formulario">
-                            <div class="form-group">
-                                <input type="text" id="titulo" class="form-control my-3" placeholder="Ingresar título">
-                            </div>
-                            <div class="form-group">
-                                <textarea id="descripcion" class="form-control my-3" placeholder="Ingresar descripción"></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="number" id="precio" class="form-control my-3" placeholder="Ingresar precio">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Agregar </button>
-                        </form>
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2 class="text-center h5">Agregar nuevo libro</h2>
+                        </div>
+                        <div class="card-body">
+                            <form id="formulario">
+                                <div class="form-group">
+                                    <input type="text" id="titulo" class="form-control my-3" placeholder="Ingresar título">
+                                </div>
+                                <div class="form-group">
+                                    <textarea id="descripcion" class="form-control my-3" placeholder="Ingresar descripción"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input type="number" id="precio" class="form-control my-3" placeholder="Ingresar precio">
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block">Agregar </button>
+                            </form>
 
+                        </div>
                     </div>
                 </div>
+                <div class="col-md-6">
+                    <table class="table caption-top bg-light">
+                        <thead>
+                        <tr>
+                            <th scope="col">Título</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Precio</th>
+                        </tr>
+                        </thead>
+                        <tbody id="tbody">
+                        <!-- <tr>
+                            <td>Yo soy Robot</td>
+                            <td>Libro de Sci-Fi</td>
+                            <td>200.00</td>
+                        </tr> -->
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-md-6">
-                <table class="table caption-top bg-light">
-                    <thead>
-                      <tr>
-                        <th scope="col">Título</th>
-                        <th scope="col">Descripción</th>
-                        <th scope="col">Precio</th>
-                      </tr>
-                    </thead>
-                    <tbody id="tbody">
-                      <tr>
-                        <td>Yo soy Robot</td>
-                        <td>Libro de Sci-Fi</td>
-                        <td>200.00</td>
-                      </tr>
-                    </tbody>
-                  </table>
-            </div>
-        </div>
         `
 leer();
 }
