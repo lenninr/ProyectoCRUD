e628a72b58ffb1765c1dd0e14d947cfae5912eac
lennin_roman <lenninr96@gmail.com>
2024-06-28 23:20:16 -0400

    Funcion Actualizar finalizada JS

--- main.js
+++ main.js
@@ -71,3 +71,39 @@ function crear(e) {
     leer();
     console.log("Libro guardado correctamente");
 }
+
+//Funcion Editar
+function editar(titulo) {
+    let libros = JSON.parse(localStorage.getItem("Libros"));
+    for (let i = 0; i < libros.length; i++) {
+        if (libros[i].titulo === titulo) {
+            document.getElementById("body").innerHTML =
+                `
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="card">
+                        <div class="card-header">
+                            <h2>Editar libro</h2>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <input type="text" id="newtitulo" class="form-control my-3" placeholder="${libros[i].titulo}">
+                                </div>
+                                <div class="form-group">
+                                    <textarea id="newdescripcion" class="form-control my-3" placeholder="${libros[i].descripcion}"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input type="number" id="newprecio" class="form-control my-3" placeholder="${libros[i].precio}">
+                                </div>
+                            </form>
+                            <button class="btn btn-success" onclick="actualizar('${i}')">Actualizar</button>
+                            <button class="btn btn-primary" onclick="VistaPrincipal()">Cancelar</button>
+                        </div>
+                    </div>
+
+            `
+
+        }
+    }
+}
